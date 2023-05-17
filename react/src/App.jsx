5aac31588c7a7c7d356870919fbfa7e1c9db229b
Abhimanyuprajapati <prajapatiabhimanyu18@gmail.com>
2023-05-17 15:07:26 +0530

    v01.21

---
+++ react/src/App.jsx
@@ -0,0 +1,18 @@
+// import Practice1 from "./useState"     // simple useState example
+// import Practice2 from "./simpleRender"    // useState count example
+// import Practice3 from "./inputRendering"   // useState simple input and display it
+import Practice4 from "./textChange"
+
+function App() {
+ 
+  return (
+    <>
+      {/* <Practice1 /> */}
+      {/* <Practice2 /> */}
+      {/* <Practice3 /> */}
+      <Practice4 />
+    </>
+  )
+}
+
+export default App
