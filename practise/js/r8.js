75d34a2f76693509ddefe9e2d338495c6de6e333
Abhimanyuprajapati <prajapatiabhimanyu18@gmail.com>
2023-05-23 01:19:11 +0530

    v01.25

---
+++ practise/js/r8.js
@@ -0,0 +1,46 @@
+// Closure
+/*
+Given an integer n, return a counter function. This counter function initially returns n and then returns 1 more than the previous value every subsequent time it is called (n, n + 1, n + 2, etc).
+
+ 
+
+Example 1:
+
+Input: 
+n = 10 
+["call","call","call"]
+Output: [10,11,12]
+Explanation: 
+counter() = 10 // The first time counter() is called, it returns n.
+counter() = 11 // Returns 1 more than the previous time.
+counter() = 12 // Returns 1 more than the previous time.
+*/
+/*
+var createCounter=function (n){
+    return function (){
+        return n++;
+    };
+};
+
+var result=createCounter(10);
+console.log(result());
+console.log(result());
+console.log(result());
+console.log(createCounter(10)());
+*/
+
+//  or 
+
+var createCounter=function (n){
+    var c=0;
+    return function (){
+        c++;
+        return n+c-1;
+    };
+};
+
+var result=createCounter(10);
+console.log(result());
+console.log(result());
+console.log(result());
+// console.log(createCounter(10)());
